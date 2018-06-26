50f2df1f12db90aef67426c6d901f103e69ca6f1
姜伟锋 <jiangweifeng@jiangweifengdeMacBook-Pro.local>
2018-06-26 16:08:02 +0800

    测试重新ss update bug

---
+++ src/main/java/io/shardingsphere/sharding/unit/dao/UserDao.java
@@ -0,0 +1,13 @@
+package io.shardingsphere.sharding.unit.dao;
+
+import io.shardingsphere.sharding.unit.model.User;
+
+import java.util.List;
+
+/**
+ * weifeng.jiang 2018-06-26 15:52
+ */
+public interface UserDao extends UserMapper {
+
+    void updateUserList(List<User> userList);
+}
---
+++ src/test/java/io/shardingsphere/sharding/test/dao/UserDaoTest.java
@@ -0,0 +1,45 @@
+package io.shardingsphere.sharding.test.dao;
+
+import io.shardingsphere.sharding.test.AppTest;
+import io.shardingsphere.sharding.unit.dao.UserDao;
+import io.shardingsphere.sharding.unit.model.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * weifeng.jiang 2018-06-26 15:43
+ */
+public class UserDaoTest extends AppTest{
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setId(10L);
+        user.setCompanyCode("testb");
+        user.setCreateTime(new Date());
+        userDao.insert(user);
+    }
+
+    @Test
+    public void testUpdateList(){
+        List<User> userList = new ArrayList<>();
+
+        User user = new User();
+        user.setId(10L);
+        user.setAge(20L);
+        userList.add(user);
+
+        User user1 = new User();
+        user1.setId(20L);
+        user1.setAge(20L);
+        userList.add(user1);
+        userDao.updateUserList(userList);
+    }
+}
